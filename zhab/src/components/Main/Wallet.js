13dac7123056f80c1b5fa3618e8cedbfa2d72d5a
Alex <alexclass98@mail.ru>
2023-05-16 12:30:01 +0300

    Счёт зависит от бека теперь

--- zhab/src/components/Main/Wallet.js
+++ zhab/src/components/Main/Wallet.js
@@ -8,19 +8,19 @@ import {Title} from "@mui/icons-material";
 
 export default function Wallet() {
     const [userId, setUserId] = useState([])
-    const [loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [accNum, setAccNum] = useState(0)
     const [accId, setAccId] = useState(0)
-    const{
+    const {
         isSubmitted,
         token,
         username,
         pk
-    } = useSelector((state)=> state.user);
+    } = useSelector((state) => state.user);
 
 
     useEffect(() => {
-        if (GetRandomAccount()===0){
+        if (GetRandomAccount() === 0) {
             fetch('http://127.0.0.1:8000/getRandomAccount/', {
                 method: "GET",
             }).then((response) => response.json()).then((resp) => {
@@ -29,16 +29,17 @@ export default function Wallet() {
         }
 
 
-    }, [ pk]);
+    }, [pk]);
     useEffect(() => {
 
-        fetch(`http://127.0.0.1:8000/user/${pk+1}/`, {
-            method: "GET"})
+        fetch(`http://127.0.0.1:8000/user/${pk + 1}/`, {
+            method: "GET"
+        })
             .then(response => response.json())
             .then((result) => {
                 setUserId(result);
                 console.log(result);
-                console.log(pk+1)
+                console.log(pk + 1)
             })
 
 
@@ -56,9 +57,9 @@ export default function Wallet() {
 
     }
 
-    async function SendKafkaMessage() {
+    async function SendKafkaMessage(authuser, account_num, balance) {
         try {
-            const response = await fetch('http://localhost:8000/grpc/?id_card=1&number_card=1234567890&cvc=123&pin=456&contract_id=789', {
+            const response = await fetch(`http://localhost:8000/grpc/?id_card=${authuser}&number_card=${account_num}&cvc=${balance}&pin=456&contract_id=789`, {
                 method: "GET",
             });
             console.log('Status:', response.status);
@@ -68,48 +69,49 @@ export default function Wallet() {
             }
             return true;
         } catch (error) {
-            console.error('Error:', error);
+            console.log('Error:', error);
             return false;
         }
     }
 
     async function CreateAccount(e) {
+        const bool = await SendKafkaMessage(pk + 1, GetRandomAccount(), 20000);
+        if (bool){
+            const formData = new FormData()
 
+            console.log(GetRandomAccount())
 
-        const formData = new FormData()
-        SendKafkaMessage()
-        console.log(GetRandomAccount())
-
-            formData.append('authuser', pk+1)
+            formData.append('authuser', pk + 1)
             formData.append('account_num', GetRandomAccount())
             formData.append('balance', 20000)
 
 
-        e.preventDefault();
-        await axios(`http://127.0.0.1:8000/account/`, {
-            method: 'POST',
-            data: formData,
+            e.preventDefault();
+            await axios(`http://127.0.0.1:8000/account/`, {
+                method: 'POST',
+                data: formData,
 
-        }).then(async (result) => {
-                console.log(result.data)
-                const formDataContract = new FormData()
-                formDataContract.append('status', 1)
-                console.log(result.data.pk)
-                formDataContract.append('account', result.data.pk)
-                formDataContract.append('auth_user', pk+1)
-                await axios(`http://127.0.0.1:8000/contract/`, {
-                    method: 'POST',
-                    data: formDataContract,
-                })
+            }).then(async (result) => {
+                    console.log(result.data)
+                    const formDataContract = new FormData()
+                    formDataContract.append('status', 1)
+                    console.log(result.data.pk)
+                    formDataContract.append('account', result.data.pk)
+                    formDataContract.append('auth_user', pk + 1)
+                    await axios(`http://127.0.0.1:8000/contract/`, {
+                        method: 'POST',
+                        data: formDataContract,
+                    })
                 }
             )
+        }
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         CreateAccount(e)
     }
-    return(
+    return (
         <div className="wallet">
             <div className='cards'>
                 <h4 className='text_wal'>Кошелек</h4>
